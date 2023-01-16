56901f748790202b897460f6b10c39e5eb10cae1
chris <chrisw@pixotech.com>
2023-01-16 13:26:32 -0600

    feat: readirect to /recipes

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,17 @@
 module.exports = (config) => {
-    config.addCollection("autocompleteOptions", function(collectionApi) {
-        const recipes = collectionApi.getFilteredByTag('recipes')
-        return recipes.map(recipe => ({label: recipe.data.title, url: recipe.url}));
-    });
+  config.addCollection("autocompleteOptions", function (collectionApi) {
+    const recipes = collectionApi.getFilteredByTag("recipes");
+    return recipes.map((recipe) => ({
+      label: recipe.data.title,
+      url: recipe.url,
+    }));
+  });
 
-    return {
-        dir: {
-            input: 'src',
-            output: 'public',
-        }
-    }
-}
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+    pathPrefix: "/recipes/",
+  };
+};
